5ff164ac20873b143a33ced24e796dc4f4dcfe64
lisalisa122051-droid <lisalisa122051@gmail.com>
2025-12-31 20:15:13 +0800

    Create help.js

---
+++ message/help.js
@@ -0,0 +1,13 @@
+module.exports = {
+  text(prefix) {
+    return `🌷 *Help* 🍬
+💗 Gunakan prefix: ${prefix} atau !
+
+Contoh:
+🫶🏻 ${prefix}menu
+🫶🏻 ${prefix}quotes
+🫶🏻 ${prefix}sticker (reply foto/video)
+
+🍬 Semangat yaa~`;
+  }
+};
