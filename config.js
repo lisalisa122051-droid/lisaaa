af1cbd7e806e136876bb371cb521dc6f2dcaee06
lisalisa122051-droid <lisalisa122051@gmail.com>
2025-12-31 20:09:41 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  botName: "Aesthetic Bot",
+  ownerNumber: ["62812xxxxxxx"], // ganti ke nomor kamu (tanpa +)
+  prefixes: [".", "!"],
+
+  // sticker watermark (metadata)
+  stickerPackname: "🌷 Aesthetic Pack",
+  stickerAuthor: "🍬 by Owner",
+
+  // mode bot
+  public: true,
+
+  // optional: limit simple
+  defaultLimit: 15,
+
+  // ffmpeg path (jika perlu custom). Biasanya di panel sudah ada "ffmpeg"
+  ffmpeg: "ffmpeg"
+};
